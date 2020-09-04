02ef0e7702acbe6dca28d684ec1f9f00c534a19a
Шоленонов Михаил <medvedsholeninov@gmail.com>
2020-09-04 17:16:54 +0300

    Change

--- CreateStruct/CreateStruct/Program.cs
+++ CreateStruct/CreateStruct/Program.cs
@@ -4,9 +4,25 @@ namespace CreateStruct
 {
     class Program
     {
+        struct Person
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public Person(string _firstName, string _lastName, int _age)
+            {
+                firstName = _firstName;
+                lastName = _lastName; age = _age;
+            }
+            public override string ToString()
+            {
+                return firstName + " " + lastName + ", age " + age;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p = new Person("Mike", "Sholeninov", 18);
+            Console.WriteLine(p);
         }
     }
 }
