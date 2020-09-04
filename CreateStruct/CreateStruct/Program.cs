da57dfe04247914e6ee4ad6bc3d1738241d2ffcc
Шоленонов Михаил <medvedsholeninov@gmail.com>
2020-09-04 17:28:05 +0300

    Change3

--- CreateStruct/CreateStruct/Program.cs
+++ CreateStruct/CreateStruct/Program.cs
@@ -2,26 +2,32 @@
 
 namespace CreateStruct
 {
-    class Program
-    {
-        struct Person
+   
+     struct Person
         {
-            public string firstName;
+        public enum Genders : int { Male, Female };
+        public string firstName;
             public string lastName;
             public int age;
-            public Person(string _firstName, string _lastName, int _age)
+            public Genders gender;
+        
+            public Person(string _firstName, string _lastName, int _age,Genders _gender)
             {
                 firstName = _firstName;
-                lastName = _lastName; age = _age;
+                lastName = _lastName;
+                age = _age;
+                gender = _gender;
             }
             public override string ToString()
             {
-                return firstName + " " + lastName + ", age " + age;
+                return firstName + " " + lastName + " ("+gender+")"+ ", age " + age;
             }
         }
+    class Program
+    {
         static void Main(string[] args)
         {
-            Person p = new Person("Mike", "Sholeninov", 18);
+            Person p = new Person("Mike", "Sholeninov", 18, Person.Genders.Male);
             Console.WriteLine(p);
         }
     }
